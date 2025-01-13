d92c864aa3c883eeb7465b470b6bfa93b306d618
Hashious <muhammed9992017@hotmail.com>
2025-01-13 13:54:52 +0100

    nur zur Probe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
         <Routes>
-            <Route path="/" element={<Login/>} />
+            <Route path="/" element={<Admin/>} />
         </Routes>
     </Router>
   );
